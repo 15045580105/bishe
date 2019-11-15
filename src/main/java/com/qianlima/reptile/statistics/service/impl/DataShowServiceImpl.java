0b67cc0e4503bc0631b0faa0a99a2beb0c2ece76
zhaoxiaoyang <zhaoxiaoyang@qianlima.com>
2019-11-15 16:22:33 +0800

    新增接口补充缺失统计数据

--- src/main/java/com/qianlima/reptile/statistics/service/impl/DataShowServiceImpl.java
+++ src/main/java/com/qianlima/reptile/statistics/service/impl/DataShowServiceImpl.java
@@ -24,24 +24,7 @@ public class DataShowServiceImpl implements DataShowService {
 
     @Override
     public Map<String, Object> dataShow(Map<String, Object> requestParams) {
-        long startTime = MapUtils.getLong(requestParams,"startTime",0L);
-        long endTime = MapUtils.getLong(requestParams,"endTime",0L);
-        if(startTime==0){
-            Calendar c = Calendar.getInstance();
-            c.set(Calendar.DAY_OF_MONTH, 1);
-            c.set(Calendar.HOUR_OF_DAY, 0);
-            c.set(Calendar.MINUTE, 0);
-            c.set(Calendar.SECOND, 0);
-            c.set(Calendar.MILLISECOND, 0);
-            startTime=c.getTimeInMillis()/1000;
-        }
-        if(endTime==0){
-            endTime=(System.currentTimeMillis()/(1000*3600*24)*(1000*3600*24)-TimeZone.getDefault().getRawOffset())/1000;
-        }
-        Criteria criteria = new Criteria();
-        criteria.andOperator(Criteria.where("currentDayTime").gte(startTime),Criteria.where("currentDayTime").lte(endTime));
-        Query query = new Query(criteria);
-        List<Map> resultList = mongoTemplate.find(query,Map.class,"phpcmsContentStatistics");
+        List<Map> resultList = getDataByMongo(requestParams);
         if(resultList==null||resultList.size()==0){
             requestParams.clear();
             requestParams.put("code",4);
@@ -78,5 +61,47 @@ public class DataShowServiceImpl implements DataShowService {
         requestParams.put("data",resultMap);
         requestParams.put("msg","返回成功");
         return requestParams;
+    }
+
+    @Override
+    public Map<String, Object> dataDisplay(Map<String, Object> requestParams) {
+        List<Map> resultList = getDataByMongo(requestParams);
+        if(resultList==null||resultList.size()==0){
+            requestParams.clear();
+            requestParams.put("code",4);
+            requestParams.put("data",null);
+            requestParams.put("msg","无数据");
+            return requestParams;
+        }
+        for(Map<String,Object> map:resultList){
+            map.remove("_id");
+        }
+        requestParams.clear();
+        
+        requestParams.put("code",0);
+        requestParams.put("data",resultList);
+        requestParams.put("msg","返回成功");
+        return requestParams;
+    }
+
+    private List<Map> getDataByMongo(Map<String, Object> requestParams){
+        long startTime = MapUtils.getLong(requestParams,"startTime",0L);
+        long endTime = MapUtils.getLong(requestParams,"endTime",0L);
+        if(startTime==0){
+            Calendar c = Calendar.getInstance();
+            c.set(Calendar.DAY_OF_MONTH, 1);
+            c.set(Calendar.HOUR_OF_DAY, 0);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.SECOND, 0);
+            c.set(Calendar.MILLISECOND, 0);
+            startTime=c.getTimeInMillis()/1000;
+        }
+        if(endTime==0){
+            endTime=(System.currentTimeMillis()/(1000*3600*24)*(1000*3600*24)-TimeZone.getDefault().getRawOffset())/1000;
+        }
+        Criteria criteria = new Criteria();
+        criteria.andOperator(Criteria.where("currentDayTime").gte(startTime),Criteria.where("currentDayTime").lte(endTime));
+        Query query = new Query(criteria);
+        return mongoTemplate.find(query,Map.class,"phpcmsContentStatistics");
     }
 }

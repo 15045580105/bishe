d2fa0dc998b61addd100c4e959954d9f6ea9ec87
mahao@qianlima.com <nznwyzdr134611>
2020-03-06 20:07:42 +0800

    定义相应码

---
+++ src/main/java/com/qianlima/reptile/statistics/entity/ResultCode.java
@@ -0,0 +1,35 @@
+package com.qianlima.reptile.statistics.entity;
+
+/**
+ * @description:
+ * @author: mahao
+ **/
+public enum ResultCode {
+    //系统
+    SUCCESS(0, "成功"),
+    PARAM_NOT_BLANK(400, "参数不能为空")
+    ;
+    private int code;
+    private String msg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    ResultCode(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+}

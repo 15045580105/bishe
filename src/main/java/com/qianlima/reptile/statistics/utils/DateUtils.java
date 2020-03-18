f052e82f34f5ecf92d3cc637bc6f9ce0b8a669e9
gaomac <15045580105@163.com>
2020-03-18 22:32:49 +0800

    统计

--- src/main/java/com/qianlima/reptile/statistics/utils/DateUtils.java
+++ src/main/java/com/qianlima/reptile/statistics/utils/DateUtils.java
@@ -19,26 +19,26 @@ public class DateUtils {
 
     public static final SimpleDateFormat FUZSDF = new SimpleDateFormat("yyyy-MM-dd");
 
-    public static String getNowFormatDateStr(){
+    public static String getNowFormatDateStr() {
         return EXACTSDF.format(new Date());
     }
 
-    public static String getFormatDateStr(Long timestmp){
-        if(timestmp == null){
+    public static String getFormatDateStr(Long timestmp) {
+        if (timestmp == null) {
             return "";
         }
         return EXACTSDF.format(new Date(timestmp));
     }
 
-    public static Date getFormatDate(Date date){
-        if(date == null){
+    public static Date getFormatDate(Date date) {
+        if (date == null) {
             return new Date();
         }
         Date parse;
         try {
             String format = EXACTSDF.format(new Date());
             parse = EXACTSDF.parse(format);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             parse = new Date();
         }
@@ -47,26 +47,27 @@ public class DateUtils {
     }
 
     /**
-     *  返回 指定格式的日期字符串
+     * 返回 指定格式的日期字符串
+     *
      * @param timestmp dateFormat
      * @return format date String
      */
-    public static String getFormatDateStr(Long timestmp, SimpleDateFormat SDF){
-        if(timestmp == null){
+    public static String getFormatDateStr(Long timestmp, SimpleDateFormat SDF) {
+        if (timestmp == null) {
             return "";
         }
         return SDF.format(new Date(timestmp));
     }
 
 
-
     /**
      * java.util.Date 返回 yyyy-MM-dd String 类型
+     *
      * @param date
      * @return format date String
      */
-    public static String getFormatDateStr(Date date){
-        if(date==null){
+    public static String getFormatDateStr(Date date) {
+        if (date == null) {
             return "";
         }
         return EXACTSDF.format(date);
@@ -75,11 +76,12 @@ public class DateUtils {
 
     /**
      * 返回 指定格式的日期字符串
+     *
      * @param date dateFormat(例如 yyyy-MM-dd)
      * @return format date String
      */
-    public static String getFormatDateStr(Date date, String dateFormat){
-        if(date==null){
+    public static String getFormatDateStr(Date date, String dateFormat) {
+        if (date == null) {
             return "";
         }
         SimpleDateFormat SDF = new SimpleDateFormat(dateFormat);
@@ -89,25 +91,27 @@ public class DateUtils {
 
     /**
      * java.lang.Integer Epoch 返回 yyyy-MM-dd String 类型
+     *
      * @param num
      * @return format date String
      */
-    public static String getFormatEpochStr(int num){
-        long time = num*1000L;
+    public static String getFormatEpochStr(int num) {
+        long time = num * 1000L;
         return EXACTSDF.format(new Date(time));
     }
 
     /**
      * yyyy-MM-dd 返回 java.util.Date
      * 不会返回null, 产生异常时返回当前时间的Date对象的实例
+     *
      * @param formatStr
      * @return
      */
-    public static Date parseDateFromDateStr(String formatStr){
+    public static Date parseDateFromDateStr(String formatStr) {
         Date date;
-        try{
+        try {
             date = EXACTSDF.parse(formatStr);
-        } catch (Exception exp){
+        } catch (Exception exp) {
             date = new Date();
         }
         return date;
@@ -115,69 +119,72 @@ public class DateUtils {
 
     /**
      * 获取自昨天12点到今天12点的int类型
+     *
      * @return {昨天12点epoch_int, 今天12点epoch_int}
      */
-    public static int[] getDate(){
+    public static int[] getDate() {
         Calendar calendar = getCalendar();
         int[] res = new int[2];
-        res[1] = (int)(calendar.getTimeInMillis()/1000);
+        res[1] = (int) (calendar.getTimeInMillis() / 1000);
         calendar.add(Calendar.DAY_OF_YEAR, -1);
-        res[0] = (int)(calendar.getTimeInMillis()/1000);
+        res[0] = (int) (calendar.getTimeInMillis() / 1000);
         return res;
     }
 
     /**
      * 获取yyyy-MM-dd 形式的前后日期时间
      * [0] prev_day [1] current day
+     *
      * @param formatDate
      * @return
      */
-    public static int[] getDate(String formatDate){
+    public static int[] getDate(String formatDate) {
         Calendar calendar = getCalendar();
         String[] dateStr = formatDate.split("-");
-        calendar.set(Integer.valueOf(dateStr[0]),Integer.valueOf(dateStr[1]) - 1,Integer.valueOf(dateStr[2]));
+        calendar.set(Integer.valueOf(dateStr[0]), Integer.valueOf(dateStr[1]) - 1, Integer.valueOf(dateStr[2]));
         int[] res = new int[2];
-        res[1] = (int)(calendar.getTimeInMillis()/1000);
+        res[1] = (int) (calendar.getTimeInMillis() / 1000);
         calendar.add(Calendar.DAY_OF_YEAR, -1);
-        res[0] = (int)(calendar.getTimeInMillis()/1000);
-        return res;
-    }
-    /**
-     * 获取某一天的上一天 12点到当天12点的int类型
-     * @return {上一天12点epoch_int, 当天12点epoch_int}
-     */
-    public static int[] getBetweenDate(int n){
-        Calendar calendar = getCalendar();
-        int[] res = new int[2];
-        calendar.add(Calendar.DAY_OF_YEAR, 0-n);
-        res[1] = (int)(calendar.getTimeInMillis()/1000);
-        calendar.add(Calendar.DAY_OF_YEAR, -1);
-        res[0] = (int)(calendar.getTimeInMillis()/1000);
+        res[0] = (int) (calendar.getTimeInMillis() / 1000);
         return res;
     }
 
     /**
-     * @param gap 数量
+     * 获取某一天的上一天 12点到当天12点的int类型
+     *
+     * @return {上一天12点epoch_int, 当天12点epoch_int}
+     */
+    public static int[] getBetweenDate(int n) {
+        Calendar calendar = getCalendar();
+        int[] res = new int[2];
+        calendar.add(Calendar.DAY_OF_YEAR, 0 - n);
+        res[1] = (int) (calendar.getTimeInMillis() / 1000);
+        calendar.add(Calendar.DAY_OF_YEAR, -1);
+        res[0] = (int) (calendar.getTimeInMillis() / 1000);
+        return res;
+    }
+
+    /**
+     * @param gap      数量
      * @param isFuture true 将来, false 过去
      * @return [Date ,expire time epoch Integer type]
      */
-    public static Object[] getEpoch(int gap, boolean isFuture){
+    public static Object[] getEpoch(int gap, boolean isFuture) {
         Calendar calendar = getCalendar();
-        gap *= isFuture ? 1: -1;
-        calendar.add(Calendar.DAY_OF_YEAR,gap);
-        return new Object[]{calendar.getTime(), (int)(calendar.getTimeInMillis()/1000)};
+        gap *= isFuture ? 1 : -1;
+        calendar.add(Calendar.DAY_OF_YEAR, gap);
+        return new Object[]{calendar.getTime(), (int) (calendar.getTimeInMillis() / 1000)};
     }
 
-    private static Calendar getCalendar(){
+    private static Calendar getCalendar() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 12);
         calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.SECOND, 0);
         return calendar;
     }
 
     /**
-     *
      * 比较两个时间的大小
      */
     public static int compareDate(String DATE1, String DATE2) {
@@ -203,10 +210,11 @@ public class DateUtils {
      * 将字符串时间格式转换成Date时间格式，参数String类型
      * 比如字符串时间："2017-12-15 21:49:03"
      * 转换后的date时间：Fri Dec 15 21:49:03 CST 2017
+     *
      * @param datetime 类型为String
      * @return
      */
-    public static Date StringToDate(String datetime,SimpleDateFormat sdFormat){
+    public static Date StringToDate(String datetime, SimpleDateFormat sdFormat) {
 //        SimpleDateFormat sdFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date date = new Date();
         try {
@@ -215,6 +223,21 @@ public class DateUtils {
             e.printStackTrace();
         }
         return date;
+    }
+
+    /**
+     * @return a
+     * @description 判断一个日期是否是本月最后一天
+     * @author gyx
+     * @date 2020-03-18 15:5
+     * @parameter * @param null
+     * @since
+     */
+    public boolean isLastDayOfMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.DAY_OF_MONTH) == calendar
+                .getActualMaximum(Calendar.DAY_OF_MONTH);
     }
 
 }

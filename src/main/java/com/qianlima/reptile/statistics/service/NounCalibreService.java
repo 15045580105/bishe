784f744fd70014aac90aaf1a3cf1776185b921b7
刘常林 <751969316@qq.com>
2020-03-20 11:59:15 +0800

    名词口径增删改接口

---
+++ src/main/java/com/qianlima/reptile/statistics/service/NounCalibreService.java
@@ -0,0 +1,17 @@
+package com.qianlima.reptile.statistics.service;
+
+import com.qianlima.reptile.statistics.entity.Response;
+
+/**
+ * @author liuchanglin
+ * @version 1.0
+ * @ClassName: NounCalibreService
+ * @date 2020/3/20 11:06 上午
+ */
+public interface NounCalibreService {
+    Response addNounCalibre(String operator,String content);
+
+    Response updateNounCalibre(String id,String operator,String content);
+
+    Response deleteNounCalibre(String id);
+}

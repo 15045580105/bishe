e0d58a02a8a0e8ab80aaa51bf814081338017a06
shixuan <shixuan@qianlima.com>
2020-03-25 21:00:34 +0800

    跑历史数据

--- src/main/java/com/qianlima/reptile/statistics/service/impl/CollectPublishTrendServiceImpl.java
+++ src/main/java/com/qianlima/reptile/statistics/service/impl/CollectPublishTrendServiceImpl.java
@@ -164,7 +164,7 @@ public class CollectPublishTrendServiceImpl implements CollectPublishTrendServic
     @Override
     public Response runHistoryData (String startDate, String endDate) {
         LocalDate startLocalDate = DateUtils.getLocalDate(startDate);
-        LocalDate endLocalDate = DateUtils.getLocalDate(startDate);
+        LocalDate endLocalDate = DateUtils.getLocalDate(endDate);
         for (LocalDate start = startLocalDate ; start.isBefore(endLocalDate.plusDays(1)) ; start = start.plusDays(1)) {
             int startTime = Integer.parseInt(Long.valueOf(DateUtils.convertTimeToLong(start.toString() + " 00:00:00") / 1000).toString());
             int endTime = Integer.parseInt(Long.valueOf(DateUtils.convertTimeToLong(start.toString() + " 23:59:59") / 1000).toString());
